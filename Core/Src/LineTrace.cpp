3f871b27a549ae5fbd14c40942424887d6050858
Haruki <underbird.h@gmail.com>
2022-03-02 17:11:39 +0900

    もどした

--- Core/Src/LineTrace.cpp
+++ Core/Src/LineTrace.cpp
@@ -77,8 +77,8 @@ float LineTrace::calcError()
 			- (line_sensor_->sensor[7] + line_sensor_->sensor[8] + line_sensor_->sensor[9] + line_sensor_->sensor[10] + line_sensor_->sensor[11] + line_sensor_->sensor[12] + line_sensor_->sensor[13]);
 	*/
 
-	float diff = (line_sensor_->sensor[2] + line_sensor_->sensor[3] + line_sensor_->sensor[4] + line_sensor_->sensor[5])
-			- (line_sensor_->sensor[8] + line_sensor_->sensor[9] + line_sensor_->sensor[10] + line_sensor_->sensor[11]);
+	float diff = (line_sensor_->sensor[3] + line_sensor_->sensor[4] + line_sensor_->sensor[5])
+			- (line_sensor_->sensor[8] + line_sensor_->sensor[9] + line_sensor_->sensor[10]);
 	mon_diff = diff;
 
 	diff = ((R_DIFF)*(diff) + (1.0 - (R_DIFF))* (pre_diff));
