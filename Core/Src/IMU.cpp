1d779ad924b989d8520c9acffcc69d93309300e0
Haruki <underbird.h@gmail.com>
2022-03-07 16:52:12 +0900

    imuのLPFもきる

--- Core/Src/IMU.cpp
+++ Core/Src/IMU.cpp
@@ -69,8 +69,8 @@ void IMU::updateValues()
 	yg_ = yg;
 	zg_ = zg;
 
-	static int16_t pre_zg;
-	zg_ = ((R_IMU)*(zg_) + (1.0 - (R_IMU))* (pre_zg)); // lowpath filter
+	//static int16_t pre_zg;
+	//zg_ = ((R_IMU)*(zg_) + (1.0 - (R_IMU))* (pre_zg)); // lowpath filter
 
 	pre_zg = zg_;
 
