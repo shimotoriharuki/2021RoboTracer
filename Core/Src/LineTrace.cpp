4ce52fc86136ec3777bf3553b149e98e8a27a60b
Haruki <underbird.h@gmail.com>
2022-10-01 15:28:55 +0900

    5走目は走るか走らないかパラメータにした

--- Core/Src/LineTrace.cpp
+++ Core/Src/LineTrace.cpp
@@ -476,11 +476,11 @@ float LineTrace::radius2Velocity(float radius)
 	}
 	else if(mode_selector_ == FIFTH_RUNNING){
 		if(radius < 400) velocity = min_velocity4_;
-		else if(radius < 500) velocity = 2.0;
-		else if(radius < 650) velocity = 2.0;
+		else if(radius < 500) velocity = 2.5;
+		else if(radius < 650) velocity = 3.0;
 		else if(radius < 1100) velocity = 3.0;
-		else if(radius < 1900) velocity = 3.0;
-		else if(radius < 2500) velocity = 3.5;
+		else if(radius < 1900) velocity = 3.3;
+		else if(radius < 2500) velocity = 4.0;
 		else velocity = max_velocity4_;
 	}
 	else velocity = 1.3;
