5e20302757eaac5109b0e00b9c14acaf6a712fb2
Haruki <underbird.h@gmail.com>
2022-11-20 14:46:14 +0900

    5走目5.0で走った．SDカードもバックアップ

--- Core/Src/LineTrace.cpp
+++ Core/Src/LineTrace.cpp
@@ -457,15 +457,17 @@ float LineTrace::dtheta2Velocity(float dtheta)
 	}
 	else if (mode_selector_ == FOURTH_RUNNING){
 		if(dtheta > 0.0025) velocity = min_velocity_; //1.0 R10
-		else if(dtheta > 0.001) velocity = 2.0; //midium radius and snake
-		else if(dtheta > 0.0004) velocity = 2.5; //midium radius and snake
+		else if(dtheta > 0.0017) velocity = 2.0; //
+		else if(dtheta > 0.0013) velocity = 2.2; // snake
+		else if(dtheta > 0.0007) velocity = 3.0; // large R
+		else if(dtheta > 0.0005) velocity = 3.5; //
 		else velocity = max_velocity3_; //5.0 large R and straight
 	}
 	else if (mode_selector_ == FIFTH_RUNNING){
 		if(dtheta > 0.0030) velocity = min_velocity_; //1.0 R10
 		else if(dtheta > 0.0017) velocity = 2.0; //
-		else if(dtheta > 0.0013) velocity = 2.2; //
-		else if(dtheta > 0.0007) velocity = 3.0; //
+		else if(dtheta > 0.0013) velocity = 2.2; // snake
+		else if(dtheta > 0.0007) velocity = 3.0; // large R
 		else if(dtheta > 0.0005) velocity = 3.5; //
 		else velocity = max_velocity4_; //6.0 straight
 	}
