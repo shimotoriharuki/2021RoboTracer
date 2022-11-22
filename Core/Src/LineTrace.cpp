3d6686cf5f9d05eb317d48f970a0d87538ea77a6
Haruki <underbird.h@gmail.com>
2022-11-22 17:59:17 +0900

    速度テーブル変更

--- Core/Src/LineTrace.cpp
+++ Core/Src/LineTrace.cpp
@@ -500,9 +500,9 @@ float LineTrace::radius2Velocity(float radius)
 	}
 	else if(mode_selector_ == FOURTH_RUNNING){
 		if(radius < 200) velocity = min_velocity3_;
-		else if(radius < 400) velocity = 2.5;
+		else if(radius < 400) velocity = 2.4;
 		else if(radius < 650) velocity = 3.0;
-		else if(radius < 1100) velocity = 3.0;
+		else if(radius < 1100) velocity = 3.3;
 		else if(radius < 1900) velocity = 3.3;
 		else if(radius < 2100) velocity = 4.0;
 		else velocity = max_velocity3_;
