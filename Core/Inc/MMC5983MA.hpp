3208749d06fb3776dafa2066f784179f9dfcbb16
Haruki <underbird.h@gmail.com>
2023-04-25 15:50:35 +0900

    とりあえずオフセットの除去を実装

--- Core/Inc/MMC5983MA.hpp
+++ Core/Inc/MMC5983MA.hpp
@@ -10,9 +10,30 @@
 
 #include "main.h"
 
+#define X_OUT0_ADDRESS 0x00
+#define X_OUT1_ADDRESS 0x01
+#define Y_OUT0_ADDRESS 0x02
+#define Y_OUT1_ADDRESS 0x03
+#define Z_OUT0_ADDRESS 0x04
+#define Z_OUT1_ADDRESS 0x05
+#define XYZ_OUT2_ADDRESS 0x06
+#define T_OUT_ADDRESS 0x07
+#define STATUS_ADDRESS 0x08
+#define INTERNAL_CONTROL0_ADDRESS 0x09
+#define INTERNAL_CONTROL1_ADDRESS 0x0A
+#define INTERNAL_CONTROL2_ADDRESS 0x0B
+#define INTERNAL_CONTROL3_ADDRESS 0x0C
+#define PRODUCT_ID1_ADDRESS 0x2f
 
 class MMC5983MA{
 private:
+	struct Offset{
+		uint16_t x;
+		uint16_t y;
+		uint16_t z;
+	};
+
+	Offset offset_;
 
 	void send(uint8_t *, uint16_t);
 	void receive(uint8_t *, uint16_t);
@@ -27,6 +48,7 @@ public:
 	void start();
 	void stop();
 
+	void calibration();
 	void updateData();
 	void getData();
 
--- Core/Src/MMC5983MA.cpp
+++ Core/Src/MMC5983MA.cpp
@@ -13,6 +13,7 @@
 
 uint8_t mon_data[2];
 uint16_t mon_xout, mon_yout;
+uint16_t mon_xout_calib, mon_yout_calib;
 HAL_StatusTypeDef mon_ret;
 
 I2C_HandleTypeDef hi2c1;
@@ -35,6 +36,9 @@ void MMC5983MA::receive(uint8_t *received_data, uint16_t size)
 
 MMC5983MA::MMC5983MA()
 {
+	offset_.x = 0;
+	offset_.y = 0;
+	offset_.z = 0;
 
 }
 
@@ -48,6 +52,7 @@ void MMC5983MA::write(uint8_t address, uint8_t *write_data, uint16_t write_data_
 	}
 
 	send(cmd, write_data_size + 1);
+	HAL_Delay(1);
 }
 
 void MMC5983MA::read(uint8_t address, uint8_t *read_data, uint16_t read_data_size)
@@ -55,33 +60,38 @@ void MMC5983MA::read(uint8_t address, uint8_t *read_data, uint16_t read_data_siz
 	send(&address, 1);
 	HAL_Delay(1);
 	receive(read_data, read_data_size);
+	HAL_Delay(1);
 }
 
 void MMC5983MA::start()
 {
 	uint8_t address = 0x09;
-	uint8_t write_data[1] = {0x01};
+	uint8_t write_data = 0x01;
 
-	write(address, write_data, 1);
-	HAL_Delay(1);
-
-	address = 0x08;
-	read(address, mon_data, 1); // Appear Status Register data on SDA line. 6-7
-	HAL_Delay(1);
-
+	write(address, &write_data, 1); //Measument start
 	HAL_Delay(1000);
 
+	// read no calibration data
 	address = 0x00;
 	read(address, mon_data, 2); // Appear Xout data on SDA line. 10-11
-	HAL_Delay(1);
-	mon_xout = mon_data[0] <<8 | mon_data[1];
+	mon_xout = mon_data[0] << 8 | mon_data[1];
 
 	address = 0x02;
 	read(address, mon_data, 2); // Appear Yout data on SDA line. 10-11
-	HAL_Delay(1);
-	mon_yout = mon_data[0] <<8 | mon_data[1];
+	mon_yout = mon_data[0] << 8 | mon_data[1];
 
 	HAL_Delay(1000);
+
+	// read calibration data
+	calibration();
+
+	address = 0x00;
+	read(address, mon_data, 2); // Appear Xout data on SDA line. 10-11
+	mon_xout_calib = ((mon_data[0] - offset_.x) << 8) | (mon_data[1] - offset_.x);
+
+	address = 0x02;
+	read(address, mon_data, 2); // Appear Yout data on SDA line. 10-11
+	mon_yout_calib = ((mon_data[0] - offset_.y) << 8) | (mon_data[1] - offset_.y);
 
 
 }
@@ -89,6 +99,46 @@ void MMC5983MA::start()
 void MMC5983MA::stop()
 {
 
+}
+
+void MMC5983MA::calibration()
+{
+	uint8_t read_x_out[2], read_y_out[2], read_z_out[2];
+
+	//Get values when device is set mode;
+	uint8_t set_cmd = 0x08;
+	write(INTERNAL_CONTROL0_ADDRESS, &set_cmd, 1); //set
+	read(X_OUT0_ADDRESS, read_x_out, 2);
+	read(X_OUT0_ADDRESS, read_y_out, 2);
+	read(X_OUT0_ADDRESS, read_z_out, 2);
+
+	uint16_t x_out_set, y_out_set, z_out_set;
+	x_out_set = read_x_out[0] << 8 | read_x_out[1];
+	y_out_set = read_y_out[0] << 8 | read_y_out[1];
+	z_out_set = read_z_out[0] << 8 | read_z_out[1];
+
+	//Get values when device is reset mode;
+	uint8_t reset_cmd = 0x10;
+	write(INTERNAL_CONTROL0_ADDRESS, &reset_cmd, 1);
+	read(X_OUT0_ADDRESS, read_x_out, 2);
+	read(X_OUT0_ADDRESS, read_y_out, 2);
+	read(X_OUT0_ADDRESS, read_z_out, 2);
+
+	uint16_t x_out_reset, y_out_reset, z_out_reset;
+	x_out_reset = read_x_out[0] << 8 | read_x_out[1];
+	y_out_reset = read_y_out[0] << 8 | read_y_out[1];
+	z_out_reset = read_z_out[0] << 8 | read_z_out[1];
+
+	//Calucurate true value excluding offset
+	int16_t x_out_H, y_out_H, z_out_H;
+	x_out_H = x_out_set - x_out_reset;
+	y_out_H = y_out_set - y_out_reset;
+	z_out_H = z_out_set - z_out_reset;
+
+	//Calucurate offset
+	offset_.x = x_out_set - x_out_H;
+	offset_.y = y_out_set - y_out_H;
+	offset_.z = z_out_set - z_out_H;
 }
 
 void MMC5983MA::updateData()
